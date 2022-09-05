cbab9a5d80ca502aa43c552b23ccdb57f58e2d07
Maria <sepkina97@mail.ru>
2022-09-05 19:44:59 +0300

    Сделала Базовую сложность

--- src/Employee.java
+++ src/Employee.java
@@ -1,71 +1,51 @@
-import java.util.Arrays;
+public class Employee {
 
-public class Main {
-
-    private static Employee[] employees = new Employee[10];
-
-    public static void main(String[] args) {
-
-        Employee ivanov = new Employee("Ivanov Ivan Petrovich", 1, 50_000);
-        Employee sidorov = new Employee("Sidorov Ivan Petrovich", 2, 80_000);
-        Employee petrova = new Employee("Petrova Inna Ivanovna", 3, 130_000);
-
-        employees[0] = ivanov;
-        employees[1] = sidorov;
-        employees[2] = petrova;
+    private String fullName;
+    private int group;
+    private int salary;
+    static int id;
 
 
-    }
-
-    public static void allEmployees() {
-        for (Employee employee : employees) {
-            System.out.println(employee);
-        }
-    }
-    public static void allFullName() {
-        for (Employee fullName : employees) {
-            System.out.println(fullName.getFullName());
-        }
-    }
-
-    public static int allSalary() {
-        int sum = 0;
-        for (Employee employee : employees) {
-            sum += employee.getSalary();
-        }
-        return sum;
-    }
-
-    public static int minSalary() {
-        int minSalary = employees[0].getSalary();
-        for (int i = 0; i < employees.length; i++) {
-            if (minSalary > employees[i].getSalary()) {
-                minSalary = employees[i].getSalary();
-            }
-        }
-        return minSalary;
-    }
-
-    public static int maxSalary() {
-        int maxSalary = employees[0].getSalary();
-        for (int i = 0; i < employees.length; i++) {
-            if (maxSalary < employees[i].getSalary()) {
-                maxSalary = employees[i].getSalary();
-            }
-        }
-        return maxSalary;
-    }
-
-    public static int mediumSalary() {
-        double mediumSalary = 0;
-        for (Employee employee : employees) {
-            mediumSalary += (double) employee.getSalary();
-        }
-        mediumSalary = mediumSalary / employees.length;
-        return (int) mediumSalary;
+    public Employee(String fullName, int group, int salary) {
+        this.fullName = fullName;
+        this.group = group;
+        this.salary = salary;
     }
 
 
+    @Override
+    public String toString() {
+        return "Employee {" +
+                "fullName = '" + fullName + '\'' +
+                ", group = " + group +
+                ", salary = " + salary +
+                ", id = " + id++ +
+                '}';
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 }
 
 
